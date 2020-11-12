1450674363577136b725fd65f47de97499346a6d
ccj1994 <a0changj2@gmail.com>
2020-11-12 21:58:46 +0800

    add

--- login/check.php
+++ login/check.php
@@ -1,38 +1,19 @@
 <?php
 $dsn="mysql:host=localhost;dbname=member;charset=utf8";
-$pdo=new PDO($dsn,'root','');
+$sql=new PDO($dsn,'root','');
 $acc=$_POST['acc'];
 $pw=$_POST['pw'];
 
 $sql="select * from `login` where `acc`='$acc' && `pw`='$pw'";
-$check=$pdo->query($sql)->fetch();
-echo $sql;
+// echo $sql;
 
+$check=$pdo->query($sql)->fetch();
 if(!empty($check)){
   echo "登入成功";
-  $member_sql="select * from `member` where `login_id`='{$check['id']}'";
-  $member=$pdo->query($member_sql)->fetch();
-  $role=$member['role'];
 
-  switch($role){
-    case '會員':
-      header('location:mem.php');
-    break;
-    case 'VIP':
-      header('location:vip.php');
-    break;
-    case '管理員':
-      header('location:admin.php');
-    break;
-  }
-}else{
   
-  header('location:index.php?meg=帳密不正確，請重新登入或註冊帳號');
-
 
 }
-
-
 
 
 ?>
