4829e02c6968b759e2437ae1a07cdaebb9ef63b0
ccj1994 <a0changj2@gmail.com>
2020-11-15 10:42:26 +0800

    完成發票清單頁面

--- invoice/invoice_list.php
+++ invoice/invoice_list.php
@@ -1,12 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>發票清單</title>
-</head>
-<body>
 
-</body>
-</html>
+<?php
+include_once "base.php";
+
+$sql="select * from `invoices` order by date desc ";
+$rows=$pdo->query($sql)->fetchALL();
+
+?>
+
+<table class="table text-center">
+<thead>
+    <tr>
+    <th>發票號碼</th>
+    <th>消費日期</th>
+    <th>消費金額</th>
+    <th>操作</th>
+    </tr>
+</thead>
+<?php
+foreach($rows as $row){
+?>
+<tbody>
+    <tr>
+        <td><?=$row['code'].$row['number'];?></td>
+        <td><?=$row['date'];?></td>
+        <td><?=$row['payment'];?></td>
+        <td>
+            <a href="?do=edit_invoice&id=<?=$row['id'];?>">
+                <button class="btn btn-primary">修改</button>
+            </a>
+            <a href="?do=del_invoice&id=<?=$row['id'];?>">
+                <button class="btn btn-danger">刪除</button>
+            </a>
+        </td>
+    </tr>
+</tbody>
+
+<?php
+}
+?>
+</table>
