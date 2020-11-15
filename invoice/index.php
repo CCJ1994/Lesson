4003d573ef3e5cc344b79a1a624c3cc776263bbd
ccj1994 <a0changj2@gmail.com>
2020-11-15 13:06:13 +0800

    完成輸入獎號功能

--- invoice/index.php
+++ invoice/index.php
@@ -6,7 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>統一發票紀錄及對獎系統</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    
+    <style>
+        .number{
+            font-weight:bolder;
+            color:red;
+        }
+    </style>
 </head>
 <body>
     <h2 class="text-center">統一發票中獎紀錄</h2>
